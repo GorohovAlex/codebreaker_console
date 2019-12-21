803e2fde96ffb64197168fd30b23b0bde42c0033
GorohovAlex <gorochov.as@gmail.com>
2019-12-21 12:17:00 +0200

    change string constant

--- spec/game_console_spec.rb
+++ spec/game_console_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe GameConsole do
       allow(game_gem).to receive(:hint_show).and_return(hint_number)
       current_subject.instance_variable_set(:@game_gem, game_gem)
       output_message = format(I18n.t('show_hint'), hint_number)
-      expect { current_subject.send(:hint_show) }.to output(output_message).to_stdout
+      expect { current_subject.send(:hint_show) }.to output(/#{output_message}/).to_stdout
     end
 
     it 'show hint ended' do
       hint_number = nil
       allow(game_gem).to receive(:hint_show).and_return(hint_number)
       current_subject.instance_variable_set(:@game_gem, game_gem)
-      expect { current_subject.send(:hint_show) }.to output("No more hints left\n").to_stdout
+      expect { current_subject.send(:hint_show) }.to output(/#{I18n.t('no_hints')}/).to_stdout
     end
   end
 end
